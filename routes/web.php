ea62071fcf0c693db163e0f6ee64625368f7318d
unknown <glennsamporna@gmail.com>
2024-02-14 01:06:40 +0800

    query builder and user model queries

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,17 +20,54 @@ Route::get('/', function () {
     // return view('welcome');
 
     // get all users / get a specific user
-    //$users = DB::select('select * from users');
+    // $users = DB::select('select * from users');
 
     // insert new user
-    //$user = DB::insert('insert into users (name, email, password) values (?,?,?)', ['Shower','scolegado@gmail.com','password']);
+    // $user = DB::insert('insert into users (name, email, password) values (?,?,?)', ['Shower','scolegado@gmail.com','password']);
 
     // update user
-    //$user = DB::update('update users set email=? where name=?',['scolegado@gmail.com','Shower']);
+    // $user = DB::update('update users set email=? where name=?',['scolegado@gmail.com','Shower']);
 
     // delete user
-    $user = DB::delete('delete from users where id=?',[2]);
+    // $user = DB::delete('delete from users where id=?',[2]);
+
+    // query builder get all users
+    // $users = DB::table('users')->where('id', 1)->get();
+
+    // query builder insert a user
+    // $user = DB::table('users')->insert([
+    //     'name'      => 'Shower',
+    //     'email'     => 'scolegado@gmail.com',
+    //     'password'  => 'password',
+    // ]);
     
+    // query builder update a user
+    // $user = DB::table('users')
+    //     ->where('id', 3)
+    //     ->update(['name' => 'May']);
+
+    // query builder delete a user
+    // $user = DB::table('users')->where('id', 3)->delete();
+
+    // user model get
+    // $user = User::where('id', 1)->first();
+
+    // user model create
+    // $user = User::create([
+    //     'name'      => 'Shower',
+    //     'email'     => 'scolegado@gmail.com',
+    //     'password'  => 'password',
+    // ]);
+
+    // user model update
+    // $user = User::where('id', 4)->update(['name' => 'May']);
+
+    // user model find
+    // $user = User::find(4);
+    
+    // user model delete
+    // $user = User::where('id', 4)->delete();
+
     dd($user);
 });
 
